299d01ca80680681461804c9613abf088ecf0bed
Kenijo <kenijo@users.noreply.github.com>
2025-11-19 15:54:57 -0800

    Externalize autoplay attributes to define them only once

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+// -------------------------
+// Shared Constants
+// -------------------------
+
+const AUTOPLAY_ATTRIBUTES = [
+    "auto_play",
+    "autoplay",
+    "autoPlay",
+    "autostart",
+    "autoStart",
+    "autostarts",
+    "autoStarts",
+    "autostartup",
+    "autoStartup",
+    "playing",
+    "playnext",
+    "playNext",
+    "playsInline",
+    "playsinline"
+];
